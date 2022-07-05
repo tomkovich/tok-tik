2a167c6b48238e2fa860cfe1f84cceb765bacdc2
tomkovich <bayern_munchen22@mail.ru>
2022-07-05 13:17:57 +0300

    Fix sidebar && add footer component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,53 +19,53 @@ const Sidebar = () => {
   return (
     <div>
       <div
-        className="block xl:hiddenm -2 ml-4 mt-3 tezt-xl"
+        className="block xl:hidden md:block m-2 ml-4 mt-3 text-xl"
         onClick={() => setShow(!isShow)}
       >
         {isShow ? <ImCancelCircle /> : <AiOutlineMenu />}
-        {isShow && (
-          <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
-            <div className="xl:border-b-2 border-gray-200 xl:pb-4">
-              <Link href="/">
-                <div className={normalLink}>
-                  <p className="text-2xl">
-                    <AiFillHome />
-                  </p>
-                  <span className="text-xl hidden xl:block">For you</span>
-                </div>
-              </Link>
-            </div>
-            {!userProfile && (
-              <div className="px-2 py-4 hidden xl:block">
-                <p className="text-gray-400">
-                  Log in to like and comment on videos
-                </p>
-                <div className="pr-4">
-                  <GoogleLogin
-                    clientId=""
-                    onSuccess={() => {}}
-                    onFailure={() => {}}
-                    cookiePolicy={"single_host_origin"}
-                    render={(renderProps) => (
-                      <button
-                        onClick={renderProps.onClick}
-                        disabled={renderProps.disabled}
-                        className="bg-white text-lg text-[#F51997] cursor-pointer border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-2 hover:text-white hover:bg-[#F51997]"
-                      >
-                        Log in
-                      </button>
-                    )}
-                  />
-                </div>
-              </div>
-            )}
-
-            <Discover />
-            <SuggestedAccounts />
-            <Footer />
-          </div>
-        )}
       </div>
+      {isShow && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="text-xl hidden xl:block">For you</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">
+                Log in to like and comment on videos
+              </p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy={"single_host_origin"}
+                  render={(renderProps) => (
+                    <button
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                      className="bg-white text-lg text-[#F51997] cursor-pointer border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-2 hover:text-white hover:bg-[#F51997]"
+                    >
+                      Log in
+                    </button>
+                  )}
+                />
+              </div>
+            </div>
+          )}
+
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
